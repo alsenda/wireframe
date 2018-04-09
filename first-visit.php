6ad4db97a930a2510c0d2a41d22ecac18de02aec
Alvaro Sendon <alvaro.sendon.aleu@gmail.com>
2018-04-09 04:59:32 +0200

    Dark design

--- first-visit.php
+++ first-visit.php
@@ -8,8 +8,11 @@
     <nav class="navbar navbar-expand-lg navbar-darker bg-darker m-0 p-0">
       <div class="container p-0">
           <ul class="list-inline m-0 p-0">
-            <li class="list-inline-item bg-lighter px-5 py-2">
+            <li class="list-inline-item bg-lighter px-5 py-2 rounded-top m-0 mt-1">
               <i class="fas fa-folder-open"></i> My documents
+            </li>
+            <li class="list-inline-item bg-dark px-5 py-2 rounded-top m-0 mt-1">
+              <i class="fas fa-map"></i> Where to print
             </li>
           </ul>
       </div>
@@ -21,25 +24,25 @@
         <div class="row">
 
         <!-- Post Content Column -->
-          <div class="col-lg-8">
+          <div class="col-lg-9">
 
 
               <button href="first-visit.php" class="btn btn-lg btn-secondary text-primary py-3 px-4">
-                <span class="h5"><i class="h3 fas fa-cloud-upload-alt"></i>
-                UPLOAD YOUR DOCUMENTS
+                <span class="h5"><i class="h3 fas fa-cloud-upload-alt mr-2"></i>
+                Upload your documents
                 </span>
               </button>
-              <div class="border bg-white border-light rounded p-2 my-2">
-                <table class="table">
+              <div class="border bg-white border-light rounded p-2 my-2 w-100">
+                <table class="table w-100">
                   <thead>
                     <tr>
-                      <th scope="col">SELECT</th>
-                      <th scope="col"></th>
-                      <th scope="col">FILE NAME</th>
-                      <th scope="col">PAGES</th>
-                      <th scope="col">STATUS</th>
-                      <th></th>
-                      <th></th>
+                      <th scope="col" style="width: 4%;">SELECT</th>
+                      <th scope="col" style="width: 4%;"></th>
+                      <th scope="col" style="width: 50%;">FILE NAME</th>
+                      <th scope="col" style="width: 6%;">PAGES</th>
+                      <th scope="col" style="width: 30%;">STATUS</th>
+                      <th scope="col" style="width: 3%;"></th>
+                      <th scope="col" style="width: 3%;"></th>
                     </tr>
                   </thead>
                   <tbody>
@@ -67,7 +70,7 @@
                 <a class="btn btn-sm text-white bg-primary py-2 px-3" href="printed.php" data-toggle="tooltip" data-placement="top" title="Try our home delivery service or go to your nearest Zerocopy printing location to get your documents for free if you are in a rush"><i class="fas fa-map"></i> Where to print</a>
               </div>
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-3">
           </div>
           <!-- /.col -->
       </div>
